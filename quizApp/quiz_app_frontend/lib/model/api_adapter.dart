c0400929f66522492307e3f38eed3e2edb0950fb
Dami Lee <50016477+iamdami@users.noreply.github.com>
2021-05-31 18:42:36 +0900

    Delete quizApp directory

--- quizApp/quiz_app_frontend/lib/model/api_adapter.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:convert';
-import 'model_quiz.dart';
-
-List<Quiz> parseQuizzes(String responseBody) {
-  final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
-  return parsed.map<Quiz>((json) => Quiz.fromJson(json)).toList();
-}
